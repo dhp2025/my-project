371bd9ca6337ada5a3200fe03f178963f9d857d7
dhp2025 <102286692+dhp2025@users.noreply.github.com>
2022-04-29 12:08:26 +0530

    Delete firebaseInit.js

--- src/firebaseInit.js
+++
@@ -1,7 +0,0 @@
-import firebase from 'firebase/app'
-//import '@firebase/firestore'
-import firebaseConfig from './firebaseConfig'
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from "firebase/firestore";
-const firebaseApp = initializeApp(firebaseConfig)
-export default getFirestore(firebaseApp)
